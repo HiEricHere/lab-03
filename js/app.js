67bcb7ff19c290051fd5bec2300146e290b6c9eb
Benjamin Clark <benisin@gmail.com>
2019-07-10 10:34:34 -0700

    got the option select working

--- js/app.js
+++ js/app.js
@@ -2,22 +2,17 @@
 
 
 function startApp() {
-  let photoInfo = loadData();
-  console.log(photoInfo);
-  // displayData(data);
+  loadData();
 
 }
 
 
-
-// get data
 function loadData() {
 
   const success = retrievedInfo => displayPage(retrievedInfo);
 
   $.get('/data/page-1.json', data => {
     if (data.length) {
-      console.log(data);
       success(data);
     } else {
       console.log('something went wrong with $.get');
@@ -25,27 +20,39 @@ function loadData() {
   }, 'json');
 }
 
-// copy html photo teplate
 
 function displayPage(data) {
+  const keywordArr = [];
+
   data.forEach(element => {
     const $newPhoto = $('#photo-template').clone();
 
     $newPhoto.find('h2').text(element.title);
     $newPhoto.find('img').attr('src', element.image_url);
     $newPhoto.find('p').text(element.description);
-
+    $newPhoto.attr('data-keyword', element.keyword);
 
     $('main').append($newPhoto);
 
+    if (!keywordArr.includes(element.keyword)) {
+      keywordArr.push(element.keyword);
+    }
+
+  });
+
+  keywordArr.forEach(element => {
+    const $newOption = $('#option-template').clone();
+    $newOption.text(element);
+    $newOption.attr('value', element);
+
+    $('select').append($newOption);
   });
 }
 
-// attach it to the DOM
-
-// remove template
-
-
+// get all the unique keyword out of returned data
+// add those keywords to the select options
+// setup listener on the select
+// hide and diplay images based on what was selected
 
 
 $(startApp);
