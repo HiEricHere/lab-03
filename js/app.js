4399fc805509aed00abfad3ea229173893c0f4c6
Benjamin Clark <benisin@gmail.com>
2019-07-10 10:11:21 -0700

    images, title, and descriptions load

--- js/app.js
+++ js/app.js
@@ -2,22 +2,49 @@
 
 
 function startApp() {
-
-  // get data
-  $.get('/data/page-1.json', data => {
-    if (data.length) {
-      console.log('success');
-    }
-  }, 'json')
-
-  // copy html photo teplate
-
-  // attach it to the DOM
-
-  // remove template
-
+  let photoInfo = loadData();
+  console.log(photoInfo);
+  // displayData(data);
 
 }
+
+
+
+// get data
+function loadData() {
+
+  const success = retrievedInfo => displayPage(retrievedInfo);
+
+  $.get('/data/page-1.json', data => {
+    if (data.length) {
+      console.log(data);
+      success(data);
+    } else {
+      console.log('something went wrong with $.get');
+    }
+  }, 'json');
+}
+
+// copy html photo teplate
+
+function displayPage(data) {
+  data.forEach(element => {
+    const $newPhoto = $('#photo-template').clone();
+
+    $newPhoto.find('h2').text(element.title);
+    $newPhoto.find('img').attr('src', element.image_url);
+    $newPhoto.find('p').text(element.description);
+
+
+    $('main').append($newPhoto);
+
+  });
+}
+
+// attach it to the DOM
+
+// remove template
+
 
 
 
