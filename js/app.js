cf7820cdb09e300eb2ff5a792907c53252ee503a
Eric Huang <ejhuang84@gmail.com>
2019-07-11 13:38:46 -0700

    we got problems

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
+const imgArray = [];
+
+
+
 function startApp() {
   loadData();
+  displayPage(imgArray);
   attachListeners();
 
 }
+let list = []
+function ImageData(rawData) {
+  // for(let key in rawData) {
+    // this[key] = rawData[key];
+  this.title = rawData.title
+  list.push(this.title)
+  // }
+}
+console.log('this is the titles',list);
+
+function loadImgArray(data){
+  data.forEach( element => {
+    imgArray.push(new ImageData(element));
+  });
+}
+
 
 function loadData() {
 
-  const success = retrievedInfo => displayPage(retrievedInfo);
+  const success = retrievedInfo => loadImgArray(retrievedInfo);
 
   $.get('./data/page-1.json', data => {
     if (data.length) {
@@ -15,25 +36,15 @@ function loadData() {
       console.log('something went wrong with $.get');
     }
   }, 'json');
+
 }
 
 function displayPage(data) {
   const keywordArr = [];
-
-
-  //if title
-  // data.sort((a,b) => {
-  //   if (a.title > b.title) return 1;
-  //   if (b.title > a.title) return -1;
-  //   return 0;
-  // });
-
-  //if horns
-  // data.sort( (a,b) => a.horns - b.horns );
-
+  console.log(imgArray);
+  console.log(typeof imgArray);
 
   data.forEach(element => {
-
     let template = $('#photoScript').html();
     let templateScript = Handlebars.compile(template);
     let context = { 'keyword' : element.keyword, 'title' : element.title, 'image_url' : element.image_url, 'description' : element.description };
@@ -51,14 +62,13 @@ function displayPage(data) {
     const $newOption = $('#option-template').clone();
     $newOption.text(element);
     $newOption.attr('value', element);
-
-    $('select').append($newOption);
+    $('#selectPics').append($newOption);
   });
 }
 
 
 function attachListeners() {
-  $('select').on('change', event => {
+  $('#selectPics').on('change', event => {
     const $choice = $(event.target).val();
     console.log($choice);
 
@@ -68,6 +78,30 @@ function attachListeners() {
       $('.photo-class').hide();
       $('.photo-class[data-keyword="' + $choice + '"]').show();
     }
+
+  });
+
+  $('#selectFilter').on('change', event => {
+    const $filter = $(event.target).val();
+    console.log($filter);
+
+    // clear out picture from DOM
+
+    // sort data
+
+    // recall render function
+
+
+
+    //if title
+    // data.sort((a,b) => {
+    //   if (a.title > b.title) return 1;
+    //   if (b.title > a.title) return -1;
+   //   return 0;
+   // });
+
+    //if horns
+   // data.sort( (a,b) => a.horns - b.horns );
 
   });
 }
